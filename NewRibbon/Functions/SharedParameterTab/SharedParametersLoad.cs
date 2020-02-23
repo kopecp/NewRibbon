dca7e5c493f7e1b6479f887d093713aba699830c
Piotr Kopec <piotrkopec@hotmail.com>
2020-02-23 13:30:47 +0100

    Stworzenie projektu na GIT Hub

---
+++ NewRibbon/Functions/SharedParameterTab/SharedParametersLoad.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+//usingi do excela
+using System.Runtime.InteropServices;
+using Excel = Microsoft.Office.Interop.Excel;
+//usingi do excela
+namespace SharedParametersLoad
+
+{
+
+    [Transaction(TransactionMode.Manual)]
+
+    [Regeneration(RegenerationOption.Manual)]
+
+    public class SharedParam: IExternalCommand
+
+    {
+
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+
+        {
+
+            //Get application and documnet objects
+
+            UIApplication uiApp = commandData.Application;
+            Document doc = uiApp.ActiveUIDocument.Document;
+            Application app = doc.Application;
+            try
+            {
+                //DO KLASY readSP
+                //Open Shared Parameter file and collect SharedParam ExteralDefinition
+                List<ExternalDefinition> spFileExternalDef = new List<ExternalDefinition>();
+                DefinitionFile spFile = doc.Application.OpenSharedParameterFile();
+                DefinitionGroups gr = spFile.Groups;
+
+                    foreach (DefinitionGroup dG in gr)
+                    {
+                            foreach (ExternalDefinition eD in dG.Definitions)
+                            {
+                                spFileExternalDef.Add(eD);
+                            }
+                    }
+                //@DO KLASY
+                //TODO: (1) powyższy kod przechwytuje ExternalDefinition z parametrów współdzielonych załadowanych do modeli. 
+                //TODO: (1) Lista która go przechwytuje jest zagnierzdzona ale to nie problem
+                //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+                //TODO: (2) Potem dopiero przejscie do tworzenia parametrów i odpowiednie ustalenie pętli tak ,żeby przypisywały się do dobrych kategorii
+                //TODO:DO KLASY readExcel
+                //Load ExcelFile and Categories to assign
+                Excel.Application xlApp = new Excel.Application();
+                Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(@"C:\Users\piotr.kopec\Downloads\testSharedParam.xls");
+                Excel._Worksheet xlWorksheet = xlWorkbook.Sheets[1];
+                Excel.Range xlRange = xlWorksheet.UsedRange;
+
+                int rowCount = xlRange.Rows.Count;
+                int colCount = xlRange.Columns.Count;
+                List<List<string>> categoryListFromExcel = new List<List<string>>();
+
+                //iterate over the rows and columns and print to the console as it appears in the file
+                //excel is not zero based!!
+                //wartości odejmowane od iteratorów wynikają z konstrukcji przykłądowego excela -> łatwiej opóścić od razu niepotrzebne kolumny i wiersze
+                for (int i = 8; i <= rowCount; i++)
+                {
+                    List<string> sublist = new List<string>();
+                    for (int j = 11; j <= colCount; j++)
+                    {
+                        sublist.Add(xlRange.Cells[i, j].Value);
+                        sublist.RemoveAll(item => item == null);
+                    }
+                    categoryListFromExcel.Add(sublist);  
+                }
+
+                //cleanup
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+
+                //rule of thumb for releasing com objects:
+                //  never use two dots, all COM objects must be referenced and released individually
+                //  ex: [somthing].[something].[something] is bad
+
+                //release com objects to fully kill excel process from running in the background
+                Marshal.ReleaseComObject(xlRange);
+                Marshal.ReleaseComObject(xlWorksheet);
+
+                //close and release
+                xlWorkbook.Close();
+                Marshal.ReleaseComObject(xlWorkbook);
+
+                //quit and release
+                xlApp.Quit();
+                Marshal.ReleaseComObject(xlApp);
+                //@DO KLASY readExcel
+                //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+                //TODO:DO KLASY category list
+                Categories categories = doc.Settings.Categories;
+
+                SortedList<string, Category> allCategories = new SortedList<string, Category>();
+
+                foreach (Category c in categories)
+                {
+                    allCategories.Add(c.Name, c);
+                }
+
+                List<List<Category>> categoryList = new List<List<Category>>();
+                foreach (List<string> sub in categoryListFromExcel)
+                {
+                    List<Category> sublist = new List<Category>();
+                    foreach (string cat in sub)
+                    {
+                        sublist.Add(allCategories[cat]);
+                    }
+                    categoryList.Add(sublist);
+                }
+
+                //@DO KLASY category list
+                //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+                //TODO:(3) generalnie to trzeba dodać jeszcze iterator do wyboru pomiędzy parametrem Type i Instance i ewentualnie grupę do któej ma zostać dodany parametr
+                Transaction trans = new Transaction(doc, "Dodanie SP");
+
+                trans.Start();
+                
+                    List<CategorySet> catSetList = new List<CategorySet>();
+                    BindingMap bindMap = doc.ParameterBindings;
+                    for (int i = 0; i < spFileExternalDef.Count; i++)
+                    {
+                        CategorySet catSet = app.Create.NewCategorySet();
+                        foreach (Category n in categoryList[i])
+                        {
+                            catSet.Insert(n);
+                        }
+                        InstanceBinding bind = app.Create.NewInstanceBinding(catSet);
+                        Array group = Enum.GetValues(typeof(BuiltInParameterGroup));
+
+                        bindMap.Insert(spFileExternalDef[i],bind);
+                    }
+                
+                trans.Commit();
+
+                return Result.Succeeded;
+            }
+
+            catch (Autodesk.Revit.Exceptions.OperationCanceledException)
+            {
+                // If user decided to cancel the operation return Result.Canceled
+                return Result.Cancelled;
+            }
+            catch (Exception ex)
+            {
+                // If something went wrong return Result.Failed
+                message = ex.Message;
+                return Result.Failed;
+            }
+        }     
+    }
+
+}
+
+
---
+++ NewRibbon/RibbonDefine.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System.Windows.Media.Imaging;
+
+namespace NewRibbon
+{
+    class RibbonDefine : IExternalApplication
+    {
+        // define a method that will create our tab and button
+
+        static void AddRibbonPanel(UIControlledApplication application)
+        {
+            // Create a custom ribbon tab
+            String tabName = "KOPEC";
+            application.CreateRibbonTab(tabName);
+
+            // Add a new ribbon panel
+            RibbonPanel ribbonPanel = application.CreateRibbonPanel(tabName, "ROBOCZA");
+            RibbonPanel ribbonPanel0 = application.CreateRibbonPanel(tabName, "Total Length");
+            RibbonPanel ribbonPanel1 = application.CreateRibbonPanel(tabName, "ShP");
+
+            // Get dll assembly path
+            string thisAssemblyPath = Assembly.GetExecutingAssembly().Location;
+
+            // create push buttons for Tabs
+            PushButtonData b1DataRobocza = new PushButtonData(
+                "cmdElementInfo",
+                "xxx" + System.Environment.NewLine + "yyy",
+                thisAssemblyPath,
+                "ElementInfo.ShowElementInfo");
+
+            PushButtonData b1DataTotalLength = new PushButtonData(
+                "cmdCurveTotalLength",
+                "Total" + System.Environment.NewLine + "  Length ",
+                thisAssemblyPath,
+                "TotalLength.CurveTotalLength");
+
+            PushButtonData b1DataShP = new PushButtonData(
+                "cmdAddSharedParam",
+                "Dodaj" + System.Environment.NewLine + "Parametry",
+                thisAssemblyPath,
+                "SharedParametersLoad.SharedParam");
+
+            //Add buttons to Ribbon --> tabs
+            PushButton pb1ElemetInfo = ribbonPanel.AddItem(b1DataRobocza) as PushButton;
+            pb1ElemetInfo.ToolTip = "Wybierz Element";
+            BitmapImage pb1ImageElementInfo = new BitmapImage(new Uri("pack://application:,,,/NewRibbon;component/Resources/AdskSample_ico.png"));
+            pb1ElemetInfo.LargeImage = pb1ImageElementInfo;
+
+            PushButton pb1TotalLength = ribbonPanel0.AddItem(b1DataTotalLength) as PushButton;
+            pb1TotalLength.ToolTip = "Wybierz linie";
+            BitmapImage pb1ImageTotalLength = new BitmapImage(new Uri("pack://application:,,,/NewRibbon;component/Resources/totalLength.png"));
+            pb1TotalLength.LargeImage = pb1ImageTotalLength;
+
+            PushButton pb1LoadShP = ribbonPanel1.AddItem(b1DataShP) as PushButton;
+            pb1TotalLength.ToolTip = "Zaladuj Parametry";
+            BitmapImage pb1ImageLoadShP = new BitmapImage(new Uri("pack://application:,,,/NewRibbon;component/Resources/add_shared_parameter.ico"));
+            pb1TotalLength.LargeImage = pb1ImageLoadShP;
+
+
+
+        }
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            // do nothing
+            // return result.succeeded
+            return Result.Succeeded;
+        }
+
+        public Result OnStartup(UIControlledApplication application)
+        {
+            // call our method that will load up our toolbar
+            // return result.succeeded
+            AddRibbonPanel(application);
+            return Result.Succeeded;
+        }
+
+    }
+}
